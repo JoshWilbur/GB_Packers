ff942e33bb3b8bc1aa07f855a85fb701d8fb3e75
JoshWilbur <joshuawilbur03@gmail.com>
2024-08-20 22:27:40 -0400

    create find_next_game function

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # GB_Packers
+
+TODO:
+* Description
+* Print that game when command 'gb_packers' is run
+* Get a build working
+* Make ascii art logo an option for command (--logo)
+* Add stats and dynamic updating (try to keep offline as much as possible)
--- parse_schedule.py
+++ parse_schedule.py
@@ -1,18 +1,34 @@
 import csv
+from datetime import datetime
 
+# Load in schedule file
 schedule_file = 'packers_schedule_2024.csv'
 
-with open(schedule_file, mode='r') as file:
-    # Create a CSV reader object
-    csv_reader = csv.DictReader(file)
 
-    # Iterate over the CSV file
-    for row in csv_reader:
-        game = row['Game']
-        date = row['Date']
-        time = row['Time_EST']
-        opponent = row['Opponent']
-        home_away = row['Home_Away']
-        
-        # Print the schedule as it is read in
-        print(f"Week {game}: {date} at {time}, vs {opponent} ({home_away})")
+# Function to obtain the closest game based off system time
+def find_next_game(csv_path):
+    next_game = None
+
+    # Obtain current time
+    now = datetime.now()
+
+    # Open csv with read permission
+    with open(csv_path, mode='r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            game_date = datetime.strptime(row['Date'], "%Y-%m-%d")
+
+            # Return game if one is found, else return none
+            if game_date >= now:
+                next_game = row
+                return next_game
+    return None
+
+
+def main():
+    nearest_game = find_next_game(schedule_file)
+    print(nearest_game)
+
+
+if __name__ == "__main__":
+    main()
