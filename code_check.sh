f047fcfa618e14c3aeb1c59968a0186e365d0876
JoshWilbur <joshuawilbur03@gmail.com>
2024-08-21 21:22:47 -0400

    fix linting script

--- code_check.sh
+++ code_check.sh
@@ -2,4 +2,6 @@
 
     poetry run black . 
     poetry run flake8 .
+    python3 -m pip install types-beautifulsoup4
+    python3 -m pip install types-requests
     poetry run mypy packers_stats.py parse_schedule.py
