210ef8bb31601a2c042ae7e29ddb97487ca89807
JoshWilbur <joshuawilbur03@gmail.com>
2024-09-15 17:51:02 -0400

    installed basic pre-commit hooks

--- code_check.sh
+++ code_check.sh
@@ -1,6 +1,6 @@
     #!/bin/bash
 
-    poetry run black . 
+    poetry run black .
     poetry run flake8 .
     poetry run python -m pip install types-beautifulsoup4 types-requests
     poetry run mypy .
